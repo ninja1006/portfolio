83abadd18056e1305ecb2c94812f239a28988d3a
AdylshaY <adylshay@gmail.com>
2025-03-12 10:34:01 +0300

    HeroBackground test

--- app/components/shared/HeroBackground.tsx
+++ app/components/shared/HeroBackground.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { motion } from 'framer-motion';
+// import { motion } from 'framer-motion';
 
 export const HeroBackground = () => {
   return (
@@ -12,7 +12,7 @@ export const HeroBackground = () => {
       <div className='absolute inset-0 bg-gradient-to-r from-purple-900/30 via-background to-indigo-900/30' />
 
       {/* Animated blobs */}
-      <motion.div
+      {/* <motion.div
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
         transition={{ duration: 0.5 }}
@@ -21,7 +21,7 @@ export const HeroBackground = () => {
         <div className='absolute -top-[300px] -left-[300px] w-[600px] h-[600px] bg-primary/30 rounded-full filter blur-[128px] opacity-50 animate-blob' />
         <div className='absolute -top-[300px] -right-[300px] w-[600px] h-[600px] bg-indigo-600/30 rounded-full filter blur-[128px] opacity-50 animate-blob animation-delay-2000' />
         <div className='absolute -bottom-[400px] left-1/2 -translate-x-1/2 w-[600px] h-[600px] bg-purple-600/30 rounded-full filter blur-[128px] opacity-50 animate-blob animation-delay-4000' />
-      </motion.div>
+      </motion.div> */}
     </div>
   );
 };
