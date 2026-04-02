b71dc930daa799c0534fafddb030ab6a1909d453
terry ito <terryito1006@gmail.com>
2026-04-02 19:09:51 +0200

    update profile image

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ const nextConfig: NextConfig = {
   /* config options here */
   distDir: process.env.NEXT_DIST_DIR || '.next',
   images: {
+    qualities: [75, 80, 85],
     remotePatterns: [
       {
         protocol: 'https',
